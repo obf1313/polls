f77dc94f71b96bc9440236c86c8d884c50066b0c
obf1313 <oubaifan@163.com>
2019-01-08 23:35:43 +0800

    test

--- polls/models.py
+++ polls/models.py
@@ -7,6 +7,8 @@ from django.utils import timezone
 # 每个模型被表示为 django.db.models.Model 的子类
 # 每个模型有一些类变量，它们都表示模型里的一个数据库字段。
 # 每个字段都是 Field 类的实例 - 比如，字符字段被表示为 CharField ，日期时间字段被表示为 DateTimeField 。
+
+
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
@@ -15,7 +17,9 @@ class Question(models.Model):
 		return self.question_text
 
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- polls/views.py
+++ polls/views.py
@@ -1,39 +1,34 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
-# from django.template import loader
+from django.views import generic
 
 from .models import Question, Choice
 
 # Create your views here.
 
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	context = {
-		'latest_question_list': latest_question_list
-	}
-	# 快捷函数render
-	return render(request, 'polls/index.html', context)
-	# return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+
+	def get_queryset(self):
+		"""Return the last five published questions."""
+		return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-	# try:
-	# 	question = Question.objects.get(pk=question_id)
-	# except Question.DoesNotExist:
-	# 	raise Http404("Question does not exist")
-	# return render(request, 'polls/detail.html', {'question': question})
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
+	# 竞争条件问题
 	question = get_object_or_404(Question, pk=question_id)
 	try:
 		selected_choice = question.choice_set.get(pk=request.POST['choice'])
@@ -45,4 +40,4 @@ def vote(request, question_id):
 	else:
 		selected_choice.votes += 1
 		selected_choice.save()
-	return HttpResponse(reverse('polls:results', args=(question.id,)))
+	return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
