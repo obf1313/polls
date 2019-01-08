2488b65cd943cd55c35ebbede2b6a62868b7f092
obf1313 <oubaifan@163.com>
2019-01-08 09:06:23 +0800

    superuser

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 # Create your models here.
+
+# 每个模型被表示为 django.db.models.Model 的子类
+# 每个模型有一些类变量，它们都表示模型里的一个数据库字段。
+# 每个字段都是 Field 类的实例 - 比如，字符字段被表示为 CharField ，日期时间字段被表示为 DateTimeField 。
+class Question(models.Model):
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+
+	def __str__(self):
+		return self.question_text
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.choice_text
