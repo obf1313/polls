40849b2b92beed84ef40dd6d3dbb0d349720720b
obf1313 <oubaifan@163.com>
2019-01-09 17:31:20 +0800

    urls change

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 # path(route,view,kwargs,name)
 urlpatterns = [
-	path('polls/', include('polls.urls')),
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
